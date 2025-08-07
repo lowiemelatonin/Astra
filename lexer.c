d95cadd368d17c6bbc538c37272cb4b3c8e69ceb
Mellow <nothornypitou@outlook.com>
2025-08-07 02:48:35 -0300

    Add files via upload

--- lexer.c
+++ lexer.c
@@ -2,6 +2,29 @@
 #include "lexer.h"
 #include <string.h>
 
+char peek(lexer *lexer){
+    return lexer->src[lexer->position];
+}
+
+char peekNext(lexer *lexer){
+    if(lexer->src[lexer->position + 1] == '\0') return '\0';
+    return lexer->src[lexer->position + 1];
+}
+
+char advance(lexer *lexer){
+    char current = lexer->src[lexer->position];
+    if(current == '\0') return '\0';
+
+    lexer->position++;
+    if(current == '\n'){
+        lexer->line++;
+        lexer->column = 1;
+    } else{
+        lexer->column++;
+    }
+    return current;
+}
+
 token createToken(lexer *lexer, token_type type, token_data data, char *lexeme){
     token token;
     token.type = type;
@@ -15,14 +38,14 @@ token createToken(lexer *lexer, token_type type, token_data data, char *lexeme){
         token.data.identifier = NULL;
         return token;
     }
-
     return token;
 }
 
-token nextToken(lexer *lexer, char *src){
-
-}
+token nextToken(lexer *lexer, char *src);
 
 void initLexer(lexer *lexer, char *src){
-
+    lexer->src = strdup(src);
+    lexer->position = 0;
+    lexer->column = 1;
+    lexer->line = 1;
 }
